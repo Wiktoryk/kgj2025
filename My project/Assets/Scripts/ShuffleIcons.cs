ec20f041f78fb280d98a34393c96e890b26cf7b5
Westesc <mikolaj.kisiel@o2.pl>
2025-04-25 23:43:22 +0200

    Prototype Scene

--- My project/Assets/Scripts/ShuffleIcons.cs
+++ My project/Assets/Scripts/ShuffleIcons.cs
@@ -1,6 +1,9 @@
 using Mono.Cecil;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class ShuffleIcons : MonoBehaviour
 {
@@ -8,6 +11,7 @@ public class ShuffleIcons : MonoBehaviour
     public List<int> icons;
     public List<IconData> generatedIcons = new List<IconData>();
     public int size;
+    public GameObject display;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,11 +24,12 @@ public class ShuffleIcons : MonoBehaviour
         {
             int i = Random.Range( 0, icons.Count );
             GameObject go = new GameObject("IconID:"+ icons[i]);
-            go.transform.parent = this.transform;
-            go.AddComponent<SpriteRenderer>();
+            go.transform.parent = display.transform;
+            go.AddComponent<Image>();
             Debug.Log(icons[i]);
             //Sprite sprite = Resources.Load<Sprite>("Assets/Sprites/"+ icons[i] + ".png");
-            //go.GetComponent<SpriteRenderer>().sprite = sprite;
+            //Sprite sprite = Resources.Load<Sprite>("Packages/com.unity.2d.sprite/Editor/ObjectMenuCreation/DefaultAssets/Textures/v2/Square.png"); 
+            go.GetComponent<Image>().color =new  Color(1,0,1);
             icons.RemoveAt( i );
         }
     }
