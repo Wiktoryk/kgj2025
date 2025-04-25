44da7431617f1044faefc849a02f062487603b17
Wiktoryk <242486@edu.p.lodz.pl>
2025-04-25 21:29:56 +0200

    update generowania przycisków

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+public struct IconData
+{
+    public int icon;
+
+    public IconData(int icon)
+    {
+        this.icon = icon;
+    }
+}
+
 public class GameController : MonoBehaviour
 {
-    public List<int> icons;
+    public List<IconData> icons;
     public GameObject buttonPreFab;
     void Start()
     {
-        icons.Add(1);
-        icons.Add(2);
         generateButtons();
     }
 
@@ -19,8 +27,10 @@ public class GameController : MonoBehaviour
 
     void generateButtons()
     {
-        foreach (int icon in icons)
+        icons = GetComponent<ShuffleIcons>().generatedIcons;
+        foreach (IconData iconData in icons)
         {
+            int icon = iconData.icon;
             GameObject button = Instantiate(buttonPreFab, new Vector3(icon, icon, 0), Quaternion.identity);
             button.SetActive(true);
             button.AddComponent<ButtonController>();
--- My project/Assets/Scripts/ShuffleIcons.cs
+++ My project/Assets/Scripts/ShuffleIcons.cs
@@ -2,16 +2,20 @@ using Mono.Cecil;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ShuffleIcons : MonoBehaviour
 {
 
     public List<int> icons;
+    public List<IconData> generatedIcons = new List<IconData>();
     public int size;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for( int i=0; i< size; i++ )
-            icons.Add( i );
+        for (int i = 0; i < size; i++)
+        {
+            icons.Add(i);
+            generatedIcons.Add(new IconData(i));
+        }
         while( icons.Count > 0) 
         {
             int i = Random.Range( 0, icons.Count );
