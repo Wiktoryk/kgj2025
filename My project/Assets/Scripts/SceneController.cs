c765bd154a75ae3b124a6b94de66f6d7b2e1cf9e
Wiktoryk <242486@edu.p.lodz.pl>
2025-04-26 05:40:33 +0200

    start screen

---
+++ My project/Assets/Scripts/SceneController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+}
